0be388944c3d7e179a15f8c5e7cfdfe8ba6c0d9f
Kalousová Marie <kalousovam@ftz.czu.cz>
2024-01-23 15:54:37 +0100

    added a lot of code

--- scripts/my_first_code.R
+++ scripts/my_first_code.R
@@ -2,3 +2,13 @@
 weight_kg <- 55
 x2 <- 3
 x2*2
+weight_kg <- sqrt(9)
+?round()
+round(3.14159)
+round(3.14159, digits=2)
+round(digits = 2, x=3.14159)
+round(2, 3.14159)
+round(3.14159, 2)
+my_function <- function(x){ x <- x + 2}
+my_result <- my_function(3)
+my_function
